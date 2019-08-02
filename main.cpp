4eec2c2817d6a8e726a1289ff8c09dedee33a76b
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 20:40:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-unsigned int a ;
-cout<<"Namber to count: ";
-cin>>a;
-for(int i=0;i<=a;i++) cout<< i<< " ";
-return 0;
+    unsigned int a ;
+    cout<<"Namber to count: ";
+    cin>>a;
+    for(int i=0;i<=a;i++) cout<< i<< " ";
+    return 0;
 }
 
